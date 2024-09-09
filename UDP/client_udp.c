58d6931a7caa57338f5f74ada5fba91bd10bf42d
Thomas Rusiecki <rusieckithomas@gmail.com>
2024-09-09 20:45:16 -0300

    udp client and server rework

--- UDP/client_udp.c
+++ UDP/client_udp.c
@@ -1,50 +1,53 @@
-
 #define _XOPEN_SOURCE 600
-#include "server_udp.h"
+#include "library.h"
 #include <netdb.h>
 #include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
     if (argc != 4)
     {
-        fprintf(stderr, "usage %s hostname port file\n", argv[0]);
+        fprintf(stderr, "Usage: %s hostname port file\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    FILE *file;                               // file descriptor
+    FILE *file = NULL;
+    int sockfd = -1;
+    bool *ack_array = NULL;
+    char *buffer = NULL;
+    struct addrinfo *res = NULL;
+
     if ((file = fopen(argv[3], "r")) == NULL) // El último parámetro es el archivo a enviar
     {
-        printf("Error al abrir el archivo\n");
-        exit(EXIT_FAILURE);
+        perror("Error al abrir el archivo");
+        goto cleanup;
     }
 
-    // clip the file name to 20 characters, after last / if it exists
+    // Clip the file name to 20 characters, after last / if it exists
     char *filename = strrchr(argv[3], '/');
     if (filename == NULL)
     {
         filename = argv[3];
     }
     else
+    {
         filename++;
+    }
 
     /*
      * Declaración de variables
      */
 
-    int sockfd, n, portno;
-
+    int portno;
     struct file_info file_info; // struct con info del archivo a enviar
-
-    struct addrinfo hints, *res;
-
-    char *buffer;
+    struct addrinfo hints;
     char reply[MTU_SIZE];
-
-    // Se podría utilizar un buffer de bits (unsigned char son 8 bits)
-    // pero por simplicidad será un buffer de booleans (8bits c/u)
-
-    bool *ack_array;
+    //response es un array de structs response en la direccion de memoria de reply
+    struct response *response = (struct response *)reply;
 
     /*
      * Obtener datos del archivo
@@ -52,177 +55,217 @@ int main(int argc, char *argv[])
     memset(&file_info, 0, sizeof(file_info));
 
     // Obtener el tamaño del archivo y el nombre
-
     fseek(file, 0, SEEK_END);
     file_info.size = ftell(file);
-
-    memcpy(file_info.name, filename, FILENAME_SIZE);
-    printf("Enviar archivo %s, tamaño %d bytes\n", file_info.name, file_info.size);
-
-    // Volver al inicio del archivo
     fseek(file, 0, SEEK_SET);
 
-    // alocar memoria en el heap para el archivo
-    if ((buffer = malloc(file_info.size)) == NULL)
+    strncpy(file_info.name, filename, FILENAME_SIZE - 1);
+    file_info.name[FILENAME_SIZE - 1] = '\0'; // Ensure null-termination
+    int const npages = (file_info.size + MAX_DATA_SIZE - 1) / MAX_DATA_SIZE;
+    
+    printf("Enviar archivo %s, tamaño %d bytes, %d páginas\n", file_info.name, file_info.size, npages);
+
+    // Alocar memoria en el heap para el archivo
+    buffer = malloc(npages * MAX_DATA_SIZE);
+    if (buffer == NULL)
     {
-        printf("Error al reservar memoria\n");
-        exit(EXIT_FAILURE);
+        perror("Error al reservar memoria");
+        goto cleanup;
     }
     memset(buffer, 0, file_info.size);
 
-    // cargar el archivo en el buffer y calcular el hash sha_256
+    // Cargar el archivo en el buffer y calcular el hash sha_256
     fread(buffer, file_info.size, 1, file);
     calculate_sha256(buffer, file_info.size, file_info.sha256_hash);
     printHex(file_info.sha256_hash);
     fclose(file);
+    file = NULL;
 
     /*
      * Calcular la cantidad de ráfagas a enviar y alocar memoria para el array de ACK
      */
+    int const burst = MAX_BURST;
 
-    // calcular la cantidad total de paginas
-    int const npages = (file_info.size + MTU_SIZE - 1) / MTU_SIZE;
-    int const burst = sizeof(struct response) / MTU_SIZE;
-
-    // malloc para el buffer de ACK
     ack_array = malloc(npages * sizeof(bool));
-
     if (ack_array == NULL)
     {
-        printf("Error al reservar memoria\n");
-        exit(EXIT_FAILURE);
+        perror("Error al reservar memoria");
+        goto cleanup;
     }
     memset(ack_array, 0, npages);
 
     /*
      * Inicializar socket UDP
      */
-
     memset(&hints, 0, sizeof hints);
     portno = atoi(argv[2]);
     hints.ai_family = AF_INET;      // IPv4
     hints.ai_socktype = SOCK_DGRAM; // UDP
 
-    // obtener info del servidor y guardar el resultado en *res
     if (getaddrinfo(argv[1], argv[2], &hints, &res) != 0)
     {
         fprintf(stderr, "Error en getaddrinfo\n");
-        exit(EXIT_FAILURE);
+        goto cleanup;
     }
 
-    // crear el socket usando el primer resultado de getaddrinfo
-    if ((sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) == -1)
+    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (sockfd == -1)
     {
         perror("Error al crear el socket");
-        exit(EXIT_FAILURE);
+        goto cleanup;
     }
 
     /*
      * Enviar metadata del archivo
      */
-
     if (sendto(sockfd, &file_info, sizeof(file_info), 0, res->ai_addr, res->ai_addrlen) == -1)
     {
         perror("Error al enviar el archivo");
-        exit(EXIT_FAILURE);
+        goto cleanup;
     }
 
-    // check if the server is ready to receive the file and answer is OK
+    // Check if the server is ready to receive the file and answer is OK
     if (recvfrom(sockfd, reply, sizeof(reply), 0, res->ai_addr, &res->ai_addrlen) == -1)
     {
         perror("Error al recibir respuesta del servidor");
-        exit(EXIT_FAILURE);
+        goto cleanup;
     }
 
-    struct response *response = (struct response *)reply;
-    if (response->pagenumber != -1 || response->ack != ACK)
+    if (response[0].ack != ACK || response[0].pagenumber != -1)
     {
-        printf("Error al recibir respuesta del servidor\n");
-        exit(EXIT_FAILURE);
+        fprintf(stderr, "Error al recibir respuesta del servidor\n");
+        goto cleanup;
     }
     else
+    {
         printf("Servidor listo para recibir archivo\n");
+    }
 
-    // una vez que el servidor esta listo para recibir, enviar el archivo de a ráfagas
+    // Una vez que el servidor esta listo para recibir, enviar el archivo de a ráfagas
     clock_t start = clock();
 
     int last_contiguous = -1;
     int iterate;
-
-    struct file_page *page = malloc(sizeof(struct file_page));
+    int current_page;
+    struct file_page page;
+    int remaining_pages = npages;
 
     do
     {
-        iterate = burst;
-        // enviar ráfaga
+        memset(reply, 0, sizeof(reply));
+        
+        iterate = (remaining_pages < burst) ? remaining_pages : burst;
+        printf("## Enviando %d páginas: ", iterate);
+
+        // Enviar ráfaga
         for (int i = 1; i <= iterate; i++)
         {
-            // if the page is not acked, send it
-            if (!ack_array[i])
-            {
-                page->pagenumber = i + last_contiguous;
-                memcpy(page->data, buffer + (i - 1) * MTU_SIZE, MTU_SIZE);
+            current_page = i + last_contiguous;
 
-                if (sendto(sockfd, page, sizeof(struct file_page), 0, res->ai_addr, res->ai_addrlen) == -1)
+            if (!ack_array[current_page])
+            {
+                page.pagenumber = current_page;
+                memcpy(page.data, buffer + current_page * MAX_DATA_SIZE, MAX_DATA_SIZE);
+
+                if (sendto(sockfd, &page, sizeof(struct file_page), 0, res->ai_addr, res->ai_addrlen) == -1)
                 {
                     perror("Error al enviar el archivo");
-                    exit(EXIT_FAILURE);
+                    goto cleanup;
                 }
+                printf(" %d,", page.pagenumber);
+
             }
             else
             {
                 iterate++;
-                break;
             }
         }
+        printf("\n");
 
-        // receive one datagram with acks for the whole burst
-        if (recvfrom(sockfd, reply, sizeof(struct response) * burst, 0, res->ai_addr, &res->ai_addrlen) == -1)
+        fd_set readfds;
+        FD_ZERO(&readfds);
+        FD_SET(sockfd, &readfds);
+        struct timeval timeout = {3, 0};
+
+        printf("Esperando respuesta del servidor\n");
+        int retval = select(sockfd + 1, &readfds, NULL, NULL, &timeout);
+
+        if (retval == -1)
         {
-            perror("Error al recibir respuesta del servidor");
-            exit(EXIT_FAILURE);
+            perror("Error en select");
+            goto cleanup;
         }
-        // iterate over the reply and set the acks in the buffer
-        for (int i = 0; i < burst; i++)
+        else if (retval == 0)
         {
-            struct response *response = (struct response *)reply + i;
-            ack_array[response->pagenumber] = response->ack;
-            if (response->ack == ACK && ack_array[response->pagenumber - 1] == ACK)
+            printf("Timeout: No se recibió respuesta del servidor en 3 segundos\n");
+        }
+        else
+        {
+            if (recvfrom(sockfd, reply, sizeof(struct response) * burst, 0, res->ai_addr, &res->ai_addrlen) == -1)
             {
-                last_contiguous = response->pagenumber;
+                perror("Error al recibir respuesta del servidor");
+                goto cleanup;
             }
         }
 
-    } while (last_contiguous < npages);
+        // Iterate over the reply until both fields are -99 (end of response)
+        int i = 0;
+        
+        printf("Respuesta del servidor:\n");
+        printf("Página %d: %s\n", response[i].pagenumber, response[i].ack == ACK ? "ACK" : "RETRY");
+            
+        while (i < burst && response[i].pagenumber != -99 && response[i].ack != -99)
+        {
+            
+            int pagenumber = response[i].pagenumber;
+            bool ack = response[i].ack == ACK;
+            ack_array[pagenumber] = ack;
+
+            if (ack)
+            {
+                remaining_pages--;
+                if (last_contiguous + 1 == pagenumber)
+                {
+                    
+                    last_contiguous++;
+                }
+            }
+            
+            i++;
+            
+        }
+
+        printf("Last contiguous: %d\n", last_contiguous);
+
+    } while (remaining_pages > 0);
 
     clock_t end = clock();
     double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("Tiempo transcurrido por conexión: %f \n", (time_spent * 1000) / 2);
+    printf("Tiempo transcurrido por conexión: %f ms\n", (time_spent * 1000) / 2);
 
-    // cleanup
-    free(buffer);
-    free(ack_array);
-    free(page);
-    freeaddrinfo(res);
-    pclose(sockfd);
-}
+    exit(EXIT_SUCCESS);
 
-void printHex(unsigned char *hash)
-{
-    int i;
-    printf("Hash: ");
-    for (i = 0; i < 32; i++)
+cleanup:
+    if (file != NULL)
     {
-        printf("%02x", hash[i]);
+        fclose(file);
     }
-    printf("\n");
-}
+    if (sockfd >= 0)
+    {
+        close(sockfd);
+    }
+    if (ack_array != NULL)
+    {
+        free(ack_array);
+    }
+    if (buffer != NULL)
+    {
+        free(buffer);
+    }
+    if (res != NULL)
+    {
+        freeaddrinfo(res);
+    }
 
-// caclcula el hash sha256 de un buffer y guarda el resultado en sha256_hash
-void calculate_sha256(const unsigned char *data, size_t data_len, unsigned char *sha256_hash)
-{
-    SHA256_CTX context;
-    SHA256_Init(&context);
-    SHA256_Update(&context, data, data_len);
-    SHA256_Final(sha256_hash, &context);
+    exit(EXIT_FAILURE);
 }
--- UDP/server_udp.c
+++ UDP/server_udp.c
@@ -1,30 +1,31 @@
 #define _XOPEN_SOURCE 600
-#include "server_udp.h"
+#include "library.h"
 #include <netdb.h>
 #include <time.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+void handle_connection(int sockfd, struct sockaddr_storage their_addr, socklen_t addr_len);
 
 int main(int argc, char *argv[])
 {
     if (argc < 2)
     {
         fprintf(stderr, "ERROR, no port provided\n");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
-    // check if port is a number from 1024 to 65535
-    if (atoi(argv[1]) < 1024 || atoi(argv[1]) > 65535)
+    // Check if port is a number from 1024 to 65535
+    int port = atoi(argv[1]);
+    if (port < 1024 || port > 65535)
     {
         fprintf(stderr, "ERROR, invalid port number\n");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
     char *portn = argv[1];
-
-    /*
-     *  un buffer para el archivo que se aloca cuando se recive el primer datagrama
-     *  un struct de tipo response para enviar la respuesta al cliente
-     * un struct de tipo file_page para recibir los datagramas
-     * un struct de tipo file_info para recibir la informacion del archivo
-     */
 
     /*
      * Networking variables
@@ -32,20 +33,7 @@ int main(int argc, char *argv[])
     int sockfd;
     struct addrinfo hints, *servinfo, *p;
     int rv;
-    int numbytes;
-    struct sockaddr_storage their_addr;
-    socklen_t addr_len;
 
-    /*
-     * Application variables
-     */
-    struct file_info file_info;
-    struct file_page file_page;
-    struct response response;
-
-    char *file_buf;
-
-    printf("Servidor corriendo en puerto %s. ", portn);
     /*
      * 1. Create a socket
      */
@@ -60,12 +48,10 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-
-    //bind on first result
+    // Bind on first result
     for (p = servinfo; p != NULL; p = p->ai_next)
     {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                             p->ai_protocol)) == -1)
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
         {
             perror("listener: socket");
             continue;
@@ -73,7 +59,7 @@ int main(int argc, char *argv[])
 
         if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1)
         {
-            pclose(sockfd);
+            close(sockfd);
             perror("listener: bind");
             continue;
         }
@@ -81,42 +67,177 @@ int main(int argc, char *argv[])
         break;
     }
 
-    if (p == NULL) {
+    if (p == NULL)
+    {
         fprintf(stderr, "listener: failed to bind socket\n");
         return 2;
     }
 
     freeaddrinfo(servinfo);
 
-    printf("Esperando conexiones...\n");
-    // 2. Receive file_info
-    addr_len = sizeof their_addr;
+    printf("Servidor corriendo en puerto %s. Esperando conexiones...\n", portn);
 
+    struct sockaddr_storage their_addr;
+    socklen_t addr_len = sizeof their_addr;
+    handle_connection(sockfd, their_addr, addr_len);
+
+    close(sockfd);
+    return 0;
+}
+
+void handle_connection(int sockfd, struct sockaddr_storage their_addr, socklen_t addr_len)
+{
+    int numbytes;
+    bool *ack_array = NULL;
+    struct file_info file_info;
+    struct file_page file_page;
+    char *file_buf = NULL;
+    char reply[MTU_SIZE];
+    struct response *response = (struct response *)reply;
+
+    // 2. Receive file_info
     if ((numbytes = recvfrom(sockfd, &file_info, sizeof(file_info), 0,
                              (struct sockaddr *)&their_addr, &addr_len)) == -1)
     {
         perror("recvfrom");
-        exit(1);
+        goto cleanup;
     }
 
-    printf("Recibiendo archivo %s, tamaño %d bytes\n", file_info.name, file_info.size);
-    
-    // 3. Send response
-    response.pagenumber = -1;
-    response.ack = ACK;
+    int const npages = (file_info.size + MAX_DATA_SIZE - 1) / MAX_DATA_SIZE;
 
-    if ((numbytes = sendto(sockfd, &response, sizeof(response), 0,
+    printf("Recibiendo archivo %s, tamaño %d bytes, %d páginas\n",
+           file_info.name, file_info.size, npages);
+
+    // 3. Send response
+    memset(&reply, 0, sizeof(reply));
+    response[0].pagenumber = -1;
+    response[0].ack = ACK;
+    printf("Aceptando archivo. Enviando respuesta al cliente\n");
+
+    if ((numbytes = sendto(sockfd, reply, sizeof(reply), 0,
                            (struct sockaddr *)&their_addr, addr_len)) == -1)
     {
         perror("talker: sendto");
-        exit(1);
+        goto cleanup;
     }
-    printf("Aceptando archivo. Enviando respuesta al cliente\n");
 
-    exit(0);
-    free(file_buf);
-    pclose(sockfd);
-    
+    // 4. Receive file and allocate memory
+    file_buf = malloc(npages * MAX_DATA_SIZE);
 
+    if (file_buf == NULL)
+    {
+        perror("malloc");
+        goto cleanup;
+    }
 
+    memset(file_buf, 0, file_info.size);
+
+    // Inicializando el buffer de acks para llevar la cuenta de las páginas recibidas correctamente
+    ack_array = malloc(npages * sizeof(bool));
+    if (ack_array == NULL)
+    {
+        perror("malloc");
+        goto cleanup;
+    }
+    for (int i = 0; i < npages; i++)
+    {
+        ack_array[i] = false;
+    }
+
+    printf("Recibiendo archivo\n");
+
+    int remaining_pages = npages;
+read: // Label para regresar a leer páginas
+
+    memset(&reply, 0, sizeof(reply));
+    memset(&file_page, 0, sizeof(file_page));
+    int received_pages = 0;
+    printf("Recibiendo página: ");
+    while (remaining_pages >= 0)
+    {
+        fd_set readfds;
+        FD_ZERO(&readfds);
+        FD_SET(sockfd, &readfds);
+        struct timeval timeout = {0, 0}; // Wait 0.1 sec
+
+        int retval = select(sockfd + 1, &readfds, NULL, NULL, &timeout);
+
+        if (retval == -1)
+        {
+            perror("select");
+            goto cleanup;
+        }
+        else if (retval == 0)
+        {
+            // No more datagrams to read
+            printf("Timeout: No se recibió respuesta del cliente en 1 segundo\n");
+            break;
+        }
+
+        // Receive file_page
+        if ((numbytes = recvfrom(sockfd, &file_page, sizeof(file_page), 0,
+                                 (struct sockaddr *)&their_addr, &addr_len)) == -1)
+        {
+            perror("recvfrom");
+            goto cleanup;
+        }
+
+        // checkeamos que no hayamos recibido anteriormente la página
+        if (ack_array[file_page.pagenumber])
+        {
+            continue;
+        }
+
+        printf("%d, ", file_page.pagenumber);
+
+        // Como se recibió la página correctamente, se marca en el buffer de acks
+        ack_array[file_page.pagenumber] = true;
+
+        // y se copia en memoria
+        size_t offset = file_page.pagenumber * MAX_DATA_SIZE;
+        memcpy(file_buf + offset, file_page.data, MAX_DATA_SIZE);
+
+        response[received_pages].pagenumber = file_page.pagenumber;
+        response[received_pages].ack = ACK;
+
+        received_pages++;
+        remaining_pages--;
+    }
+    //
+
+    response[received_pages].pagenumber = -99;
+    response[received_pages].ack = -99;
+
+    // Send response to client
+    if ((numbytes = sendto(sockfd, reply, sizeof(reply), 0,
+                           (struct sockaddr *)&their_addr, addr_len)) == -1)
+    {
+        perror("talker: sendto");
+        goto cleanup;
+    }
+    printf("remaining pages: %d\n", remaining_pages);
+
+    if (remaining_pages > 0)
+    {
+        goto read;
+    }
+
+    // 5. Calculate hash and compare
+    unsigned char hash[HASH_SIZE];
+    calculate_sha256(file_buf, file_info.size, hash);
+    printf("Hash calculado: ");
+    printHex(hash);
+    printf("Hash recibido: ");
+    printHex(file_info.sha256_hash);
+    compareHash(hash, file_info.sha256_hash);
+
+cleanup:
+    if (file_buf != NULL)
+    {
+        free(file_buf);
+    }
+    if (ack_array != NULL)
+    {
+        free(ack_array);
+    }
 }
