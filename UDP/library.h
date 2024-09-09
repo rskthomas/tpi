7c2776aad881a54a37ac7b1e4b17359a6b024a67
Thomas Rusiecki <rusieckithomas@gmail.com>
2024-09-09 20:44:52 -0300

    library and header file name changed

--- UDP/library.h
+++ UDP/library.h
@@ -1,19 +1,23 @@
-#ifndef SERVER_H_
-#define SERVER_H_
+#ifndef LIBRARY_H_
+#define LIBRARY_H_
 
 #include <openssl/sha.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <sys/select.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <netinet/in.h>
+
 #define HASH_SIZE 32
 #define FILENAME_SIZE 50
+#define MAX_BURST 20
 
 #define MTU_SIZE 512
+#define MAX_DATA_SIZE (MTU_SIZE - sizeof(unsigned int))
 
 void calculate_sha256(const unsigned char *data, size_t data_len, unsigned char *sha256_hash);
 void printHex(unsigned char *hash);
@@ -30,8 +34,8 @@ struct file_info
 
 struct file_page
 {
+    char data[MAX_DATA_SIZE];
     unsigned int pagenumber;
-    char data[MTU_SIZE - sizeof(unsigned int)];
 };
 
 struct response {
